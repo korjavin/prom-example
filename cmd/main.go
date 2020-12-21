d459667eb861fd9c157feda7a7a07ad8eb76731d
Sergey Ryabov <reallife@pm.me>
2020-12-21 17:06:35 +0500

    Added some metrics

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"math/rand"
 	"net/http"
+	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
@@ -17,22 +19,40 @@ const (
 )
 
 type Metrics struct {
-	calls prometheus.Counter
+	calls             prometheus.Counter
+	sliceSize         prometheus.Gauge
+	duration          prometheus.Summary
+	durationHistogram prometheus.Histogram
 }
 
 var metrics = &Metrics{
-	calls: promauto.NewCounter(prometheus.CounterOpts{Namespace: system, Name: "superjob_calls"}),
+	calls:     promauto.NewCounter(prometheus.CounterOpts{Namespace: system, Name: "superjob_calls"}),
+	sliceSize: promauto.NewGauge(prometheus.GaugeOpts{
+		Namespace:   system,
+		Name:        "superjob_slice_size",
+	}),
+	duration: promauto.NewSummary(prometheus.SummaryOpts{
+		Namespace:  system,
+		Name:       "superjob_calls_duration_summary",
+		Objectives: map[float64]float64{0.5: 0.05, 0.9: 0.01, 0.99: 0.001},
+	}),
+	durationHistogram: promauto.NewHistogram(prometheus.HistogramOpts{
+		Namespace: system,
+		Name:      "superjob_calls_duration_histogram",
+		Buckets:   []float64{.005, .01, .025, .05, .1, .25, .5, 1, 2.5, 5, 10},
+	}),
 }
 
 func main() {
 	http.Handle("/metrics", promhttp.Handler())
 	http.HandleFunc("/superjob", superhandler)
+	log.Printf("Server listening %s", listen)
 	http.ListenAndServe(listen, nil)
 }
 
 // TODO measure me
 func superhandler(w http.ResponseWriter, r *http.Request) {
-	metrics.calls.Add(1)
+	start := time.Now()
 	size := rand.Int31n(max)
 	slice := make([]int, size)
 	for idx := range slice {
@@ -40,4 +60,11 @@ func superhandler(w http.ResponseWriter, r *http.Request) {
 	}
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(fmt.Sprintf("Len %d", len(slice))))
+
+	duration := time.Since(start)
+
+	metrics.calls.Add(1)
+	metrics.sliceSize.Set(float64(size))
+	metrics.duration.Observe(float64(duration.Milliseconds()))
+	metrics.durationHistogram.Observe(float64(duration.Milliseconds()))
 }
